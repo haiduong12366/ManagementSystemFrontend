7bb9cd90bea7f7090a5007be370c80216ef90024
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2024-07-17 14:53:44 +0700

    link

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:9090"
+export const API_BASE_URL = "http://managementsystem-production.up.railway.app"
 
 const Api =(jwt)=>{
     const api = axios.create({baseURL:API_BASE_URL})
